bb7a80bcc021e9f64d9705d128ead259f96b6b73
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-31 11:49:12 +0530

    Add project files.

---
+++ static and instance class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_and_instance_class
+{
+
+    class Circle
+    {
+        float _PI = 3.14F;
+        int _Radius;
+
+        public Circle(int Radius)
+        {
+            this._Radius = Radius;
+        }
+
+        public float CalculateArea ()
+        {
+            return this._PI * this._Radius * this._Radius;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(7);
+            float Area = c1.CalculateArea();
+            Console.WriteLine("Area of the circle is = {0}", Area);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ static and instance class/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("static and instance class")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("static and instance class")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("434c9e05-fc86-4ad7-a297-2c7a1dd92ade")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
