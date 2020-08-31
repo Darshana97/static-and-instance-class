15d8017b9fc026ab38db28b5f15aae6aacb6dc5e
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-31 17:24:09 +0530

    static & instance class constructor

--- static and instance class/Program.cs
+++ static and instance class/Program.cs
@@ -9,17 +9,28 @@ namespace static_and_instance_class
 
     class Circle
     {
-        float _PI = 3.14F;
+        static float _PI;
         int _Radius;
+
+        
 
         public Circle(int Radius)
         {
+            Console.WriteLine("Instance constructor called");
             this._Radius = Radius;
+        }
+
+        static Circle()
+        {
+            Console.WriteLine("Static constructor called");
+            Circle._PI = 3.141F;
+
         }
 
         public float CalculateArea ()
         {
-            return this._PI * this._Radius * this._Radius;
+            // return this._PI * this._Radius * this._Radius;
+            return Circle._PI * this._Radius * this._Radius;
         }
 
     }
@@ -28,8 +39,13 @@ namespace static_and_instance_class
         static void Main(string[] args)
         {
             Circle c1 = new Circle(7);
-            float Area = c1.CalculateArea();
-            Console.WriteLine("Area of the circle is = {0}", Area);
+            float Area1 = c1.CalculateArea();
+            Console.WriteLine("Area of the circle is = {0}", Area1);
+            
+
+            Circle c2 = new Circle(10);
+            float Area2 = c2.CalculateArea();
+            Console.WriteLine("Area of the circle is = {0}", Area2);
             Console.ReadLine();
 
 
